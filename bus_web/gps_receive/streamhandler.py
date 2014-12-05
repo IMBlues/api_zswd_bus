94469110634400a3992f0eeafcd027ff393db14d
BluesYu <yuxinyuanshiblues@gmail.com>
2014-12-05 22:30:04 +0800

    adding exception handler

--- bus_web/gps_receive/streamhandler.py
+++ bus_web/gps_receive/streamhandler.py
@@ -1,10 +1,9 @@
 #! -*- coding:utf-8 -*-
-import traceback
 import struct
 import urllib
 from lib.hexadecimal_deal import DataStruct
 from bus.models import *
-
+from gps_settings import *
 from SocketServer import StreamRequestHandler
 
 
@@ -20,108 +19,15 @@ class BusStreamRequestHandler(StreamRequestHandler):
 
         if self.packed_data[0] == 104 and self.packed_data[1] == 104:
             pass
-#            print 'ok'
+
         elif self.packed_data[0] == 0 and self.packed_data[1] == 0:
             response_str = 'bbbbb'
             response_data = struct.pack(response_str, 84, 104, 26, 13, 10)
             self.request.sendall(response_data)
             print 'return right'
+
         else:
             pass
-
-    def update_bus_coordinate(self, data, bus):
-        '''
-        更新车辆坐标
-        '''
-
-        coordinate = Coordinate(longitude=data['longitude'],
-        latitude=data['latitude'],bus_number=data['bus_number'])
-        coordinate.save()
-        bus.coordinate = coordinate
-        bus.save()
-
-    def update_bus_stop(self, data, bus):
-        '''
-        更新车站
-        '''
-
-        MAX_LENGTH = 21000000
-
-        stops = Stop.objects.filter(route=bus.route)
-        distance = MAX_LENGTH
-        for stop in stops:
-            temp_distance = ((bus.coordinate.latitude - stop.latitude)**2 +
-                             (bus.coordinate.longitude - stop.longitude)**2)
-            if distance < temp_distance:
-                bus.stop = stop
-        bus.save()
-
-    def update_bus_route(self, data, bus):
-        '''
-        更新车辆路线
-        '''
-
-        ERROR_VALUE =0.00007
-
-        routes = Route.objects.all()
-        for r in routes:
-            if (abs(r.special_coordinate.latitude - data['latitude'])<ERROR_VALUE)\
-                and (abs(r.special_coordinate.longitude -
-                data['longitude']<ERROR_VALUE)):
-
-                bus.route = r
-        bus.save()
-
-    def register_new_bus(self,data):
-#        if SpecialCoordinate.objects.filter(route_name='TEST'):
-#            special_coordinate = SpecialCoordinate.objects.get(route_name='TEST')
-#        else:
-#            special_coordinate = SpecialCoordinate(longitude=114.332141, latitude=30.520142, route_name='TEST')
-#            special_coordinate.save()
-#
-#        if Route.objects.filter(final_stop="TEST2"):
-#            route = Route.objects.get(departure_stop='TEST1', final_stop='TEST2',special_coordinate = special_coordinate)
-#        else:
-#            route = Route.objects.get(final_stop='TEST2')
-#            route.save()
-#
-#        if Stop.objects.filter(name='TEST'):
-#            stop = Stop.objects.get(name='TEST')
-#        else:
-#            stop = Stop(name='TEST', route=route, longitude=114.233333, latitude=30.520212)
-#            stop.save()
-        try:
-            coordinate = Coordinate.objects.all()[0]
-        except:
-            coordinate = Coordinate(longitude=114.233333, latitude=30.5312333)
-            coordinate.save()
-        route = Route.objects.all()[0]
-        stop = Stop.objects.filter(route=route)[0]
-        bus = Bus(route=route, stop=stop, coordinate=coordinate, number=data['bus_number'])
-        bus.save()
-        return bus
-
-    def save(self, data):
-        '''
-        数据库存储
-        '''
-        bus = str()
-        try:
-            if Bus.objects.filter(number=data['bus_number']):
-                bus = Bus.objects.get(number=data['bus_number'])
-            else:
-                bus = self.register_new_bus(data)
-                self.update_bus_coordinate(data, bus)
-                self.update_bus_route(data, bus)
-            if bus.route:
-                self.update_bus_stop(data, bus)
-        except:
-            pass
-
-    def temp_save(self, data):
-        test_coordinate = TestCoordinate(latitude=data['latitude'], longitude=data['longitude'])
-        test_coordinate.save()
-        print 'time:', test_coordinate.time, '; lat:', test_coordinate.latitude, '; lont:', test_coordinate.longitude
 
     def packdata(self, data):
 
@@ -152,7 +58,7 @@ class BusStreamRequestHandler(StreamRequestHandler):
             print 'live_confirm'
             return (0, 0)
 
-        form_string = 'B' * len(data) 
+        form_string = 'B' * len(data)
         try:
             packed_data = struct.unpack(form_string, data)
         except:
@@ -198,6 +104,100 @@ class BusStreamRequestHandler(StreamRequestHandler):
 
         return packed_data
 
+    @staticmethod
+    def update_bus_coordinate(data, bus):
+        '''
+        更新车辆坐标
+        '''
+
+        coordinate = Coordinate(longitude=data['longitude'],
+                                latitude=data['latitude'], bus_number=data['bus_number'])
+        coordinate.save()
+        bus.coordinate = coordinate
+        bus.save()
+
+    @staticmethod
+    def update_bus_stop(self, data, bus):
+        '''
+        更新车站
+        '''
+
+        stops = Stop.objects.filter(route=bus.route)
+        distance = MAX_LENGTH
+        for stop in stops:
+            temp_distance = ((bus.coordinate.latitude - stop.latitude)**2 +
+                             (bus.coordinate.longitude - stop.longitude)**2)
+            if distance < temp_distance:
+                bus.stop = stop
+        bus.save()
+
+    @staticmethod
+    def update_bus_route(data, bus):
+        '''
+        更新车辆路线
+        '''
+
+        routes = Route.objects.all()
+        for r in routes:
+            if (abs(r.special_coordinate.latitude - data['latitude']) < ERROR_VALUE) \
+                    and (abs(r.special_coordinate.longitude - data['longitude'] < ERROR_VALUE)):
+
+                bus.route = r
+        bus.save()
+
+    @staticmethod
+    def register_new_bus(data):
+#        if SpecialCoordinate.objects.filter(route_name='TEST'):
+#            special_coordinate = SpecialCoordinate.objects.get(route_name='TEST')
+#        else:
+#            special_coordinate = SpecialCoordinate(longitude=114.332141, latitude=30.520142, route_name='TEST')
+#            special_coordinate.save()
+#
+#        if Route.objects.filter(final_stop="TEST2"):
+#            route = Route.objects.get(departure_stop='TEST1', final_stop='TEST2',special_coordinate = special_coordinate)
+#        else:
+#            route = Route.objects.get(final_stop='TEST2')
+#            route.save()
+#
+#        if Stop.objects.filter(name='TEST'):
+#            stop = Stop.objects.get(name='TEST')
+#        else:
+#            stop = Stop(name='TEST', route=route, longitude=114.233333, latitude=30.520212)
+#            stop.save()
+        try:
+            coordinate = Coordinate.objects.all()[0]
+        except:
+            coordinate = Coordinate(longitude=114.233333, latitude=30.5312333)
+            coordinate.save()
+        route = Route.objects.all()[0]
+        stop = Stop.objects.filter(route=route)[0]
+        bus = Bus(route=route, stop=stop, coordinate=coordinate, number=data['bus_number'])
+        bus.save()
+        return bus
+
+    def save(self, data):
+        '''
+        数据库存储
+        '''
+        bus = str()
+        try:
+            if Bus.objects.filter(number=data['bus_number']):
+                bus = Bus.objects.get(number=data['bus_number'])
+            else:
+                bus = self.register_new_bus(data)
+                self.update_bus_coordinate(data, bus)
+                self.update_bus_route(data, bus)
+            if bus.route:
+                self.update_bus_stop(data, bus)
+        except:
+            pass
+
+    @staticmethod
+    def temp_save(data):
+        test_coordinate = TestCoordinate(latitude=data['latitude'], longitude=data['longitude'])
+        test_coordinate.save()
+        print 'time:', test_coordinate.time, '; lat:', test_coordinate.latitude, '; lont:', test_coordinate.longitude
+
     def handle(self):
 
         while True:
@@ -207,9 +207,8 @@ class BusStreamRequestHandler(StreamRequestHandler):
                     print "the startup 1024 bytes of data is empty"
                 else:
                     print "len(data): %d" % (len(data))
-                    print data
                     self.packed_data = self.packdata(data)
                     self.judge_data_type()
-            except:
-                traceback.print_exc()
+            except Exception as ex:
+                print "Exception in receiving:", ex
                 break
--- bus_web/lib/hexadecimal_deal.py
+++ bus_web/lib/hexadecimal_deal.py
@@ -6,6 +6,6 @@ __author__ = 'gpxlcj'
 class DataStruct:
 
     data_type = ''
-    
+
     def __init__(self, temp_type):
         data_type = temp_type
